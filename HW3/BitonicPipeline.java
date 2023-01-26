5c8444730aafbf01c63d7d365ae2254496bf880b
Noha Nomier <nnomier@Nohas-MacBook-Pro.local>
2023-01-25 23:20:18 -0800

    [BitonicSort] Cleanup

--- HW3/BitonicPipeline.java
+++ HW3/BitonicPipeline.java
@@ -1,99 +1,128 @@
+/**
+ * The BitonicPipeline class creates a pipeline of worker threads that sort an
+ * array of double values using the bitonic sort algorithm.
+ * The pipeline is divided into 3 stages:
+ * RandomArrayGenerator threads generate random arrays of double values
+ * StageOne threads sort the arrays generated in stage 1
+ * BitonicStage threads merge and sort the arrays from stage 2 using BitonicSorting
+ * The class also has a runPipeline() method that initializes the threads, runs the pipeline
+ * and evaluates the sorting process.
+ * @author Noha Nomier
+ */
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
 public class BitonicPipeline {
-    public static final int N = 1 << 22;  // size of the final sorted array (power of two)
-    public static final int TIME_ALLOWED = 10;  // seconds
-    public static final int ARRAY_GENERATOR_THREADS = 4;
-    public static final int STAGE_ONE_THREADS = ARRAY_GENERATOR_THREADS;
-    public static final int BITONIC_STAGE_THREADS = 2;
+    private List<SynchronousQueue<double[]>> dataQueues;
+    private Thread[] workerThreads;
+    private static final int N = 1 << 22;  // size of the final sorted array (power of two)
+    private static final int TIME_ALLOWED = 10;  // seconds
+    private static final int ARRAY_GENERATOR_THREADS = 4;
+    private static final int STAGE_ONE_THREADS = ARRAY_GENERATOR_THREADS;
+    private static final int BITONIC_STAGE_THREADS = 2;
+    private static final int TOTAL_THREADS = ARRAY_GENERATOR_THREADS + STAGE_ONE_THREADS + BITONIC_STAGE_THREADS + 1; // add 1 for last stage
 
-    public static final int TOTAL_THREADS = ARRAY_GENERATOR_THREADS + STAGE_ONE_THREADS + BITONIC_STAGE_THREADS + 1; // add 1 for last stage
-
-
-    public static void main(String[] args) throws InterruptedException {
-        // Create SynchronousQueue objects for each pipeline stage
-            long start = System.currentTimeMillis();
-
-            List<SynchronousQueue<double[]>> dataQueues = new ArrayList<>();
-
-            Thread[] workerThreads = new Thread[TOTAL_THREADS];
-            for (int i = 0; i < TOTAL_THREADS; i++) {
-                dataQueues.add(new SynchronousQueue<>());
-            }
-            for (int i = 0; i < ARRAY_GENERATOR_THREADS; i++) {
-                workerThreads[i] = new Thread(new RandomArrayGenerator(N / 4, dataQueues.get(i)));
-            }
-
-
-            for (int i = 0; i < STAGE_ONE_THREADS; i++) {
-                int index = i + ARRAY_GENERATOR_THREADS;
-                workerThreads[index] = new Thread(new StageOne(dataQueues.get(i), dataQueues.get(index), "THREAD ["+index+"]"));
-            }
-
-            for (int i = 0; i < BITONIC_STAGE_THREADS; i++) {
-                int stageOneIndex = (i*2) + ARRAY_GENERATOR_THREADS;
-                int index = i + ARRAY_GENERATOR_THREADS + STAGE_ONE_THREADS;
-//                System.out.println("index is" + index);
-                workerThreads[index] = new Thread(new BitonicStage(dataQueues.get(stageOneIndex), dataQueues.get(stageOneIndex+1), dataQueues.get(index), "THREAD[" + index + "]"));
-            }
-
-            workerThreads[TOTAL_THREADS-1] = new Thread(new BitonicStage(dataQueues.get(TOTAL_THREADS-3), dataQueues.get(TOTAL_THREADS-2), dataQueues.get(TOTAL_THREADS-1), "THREAD[" + (TOTAL_THREADS - 1) + "]"));
-            for (int i = 0; i < TOTAL_THREADS; i++) {
-                workerThreads[i].start();
-            }
-
-
-//        double[] data = new double[2];
-        int count = 0;
-        while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
-            double [] res = dataQueues.get(10).poll(10 * 1000, TimeUnit.MILLISECONDS);
-            if( res != null) {
-                if (!RandomArrayGenerator.isSorted(res) || N != res.length)
-                    System.out.println("failed");
-                count++;
-                System.out.println("elarray" + res[0]);
-            }
-
-
-        }
-
-        System.out.println("total= " + count);
-//        for (int i = 0; i < 3; i++) {
-//
-//
-//        }
-//        System.out.println(Arrays.toString(data));
-
-//        for (int i = 0; i < ARRAY_GENERATOR_THREADS; i++) {
-//                workerThreads[i].join();
-//            }
-
-
-//        for (int i = 0; i < ARRAY_GENERATOR_THREADS; i++) {
-//            workerThreads[i].interrupt();
-//        }
-            // Start the main thread
-//        long start = System.currentTimeMillis();
-//        int work = 0;
-//        while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
-//            // Wait for the output from the final pipeline thread
-//            double[] sortedArray = outputQueue6.take();
-//
-//            // Check if the array is sorted and discard it if not
-//            if (!RandomArrayGenerator.isSorted(sortedArray) || N != sortedArray.length) {
-//                System.out.println("failed");
-//            } else {
-//                work++;
-//            }
-//        }
-//        System.out.println("sorted " + work + " arrays (each: " + N + " doubles) in " + TIME_ALLOWED + " seconds");
-//
-//        // Shut down the threads
-        }
-
+    public BitonicPipeline() {
+        dataQueues = new ArrayList<>();
+        workerThreads = new Thread[TOTAL_THREADS];
     }
 
+    /**
+     * Runs the pipeline and performs the sorting.
+     *
+     * @throws InterruptedException
+     */
+    public void runPipeline() throws InterruptedException {
+        initializeThreads();
+        evaluateSorting();
+        threadCleanup();
+    }
+
+    /**
+     * Initializes the worker threads for each pipeline stage and
+     * creates SynchronousQueue objects to pass data between stages
+     */
+    private void initializeThreads() {
+        // Create SynchronousQueue objects and worker threads for each pipeline stage
+
+        for (int i = 0; i < TOTAL_THREADS; i++) {
+            dataQueues.add(new SynchronousQueue<>());
+        }
+
+        // Initialize RandomArrayGenerator Threads to generate random array sections
+        for (int i = 0; i < ARRAY_GENERATOR_THREADS; i++) {
+            workerThreads[i] = new Thread(new RandomArrayGenerator(N / 4, dataQueues.get(i)));
+        }
+
+        // Initialize StageOne Threads to sort the generated arrays
+        for (int i = 0; i < STAGE_ONE_THREADS; i++) {
+            int index = i + ARRAY_GENERATOR_THREADS;
+            workerThreads[index] = new Thread(new StageOne(dataQueues.get(i), dataQueues.get(index),
+                    "THREAD [" + index + "]"));
+        }
+
+        // Initialize BitonicStage Threads
+        for (int i = 0; i < BITONIC_STAGE_THREADS; i++) {
+            int stageOneIndex = (i * 2) + ARRAY_GENERATOR_THREADS;
+            int index = i + ARRAY_GENERATOR_THREADS + STAGE_ONE_THREADS;
+            workerThreads[index] = new Thread(new BitonicStage(dataQueues.get(stageOneIndex),
+                    dataQueues.get(stageOneIndex + 1), dataQueues.get(index),
+                    "THREAD[" + index + "]"));
+        }
+
+        // Initialize last BitonicStage Thread to sort the final sequence
+        int finalStageIndex = TOTAL_THREADS - 1;
+        workerThreads[finalStageIndex] = new Thread(new BitonicStage(dataQueues.get(finalStageIndex - 2),
+                dataQueues.get(finalStageIndex - 1), dataQueues.get(finalStageIndex),
+                "THREAD[" + finalStageIndex + "]"));
+    }
+
+    /**
+     * Evaluates the sorting by checking the results obtained from the final queue
+     * @throws InterruptedException
+     */
+    private void evaluateSorting() throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        // Start threads
+        for (int i = 0; i < TOTAL_THREADS; i++) {
+            workerThreads[i].start();
+        }
+
+        int numberOfSortedArrays = 0;
+        boolean successful = true;
+        while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
+            double[] res = dataQueues.get(10).poll(10 * 1000, TimeUnit.MILLISECONDS);
+            if (res != null) {
+                if (!RandomArrayGenerator.isSorted(res) || N != res.length) {
+                    System.out.println("Sorting was not successfull");
+                    successful = false;
+                    break;
+                }
+                numberOfSortedArrays++;
+            }
+        }
+        if (successful)
+            System.out.println("sorted " + numberOfSortedArrays + " arrays (each: " + N + " doubles) in "
+                    + TIME_ALLOWED + " seconds");
+    }
+
+    /**
+     * Interrupts all threads that are still running
+     */
+    private void threadCleanup() {
+        for (Thread thread : this.workerThreads) {
+            if (thread.isAlive()) {
+                thread.interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        BitonicPipeline pipeline = new BitonicPipeline();
+        pipeline.runPipeline();
+    }
+}
--- HW3/BitonicStage.java
+++ HW3/BitonicStage.java
@@ -1,8 +1,16 @@
+/**
+ * A class that performs the bitonic sort algorithm on a given input.
+ * The input is divided into two halves both sorted ascendingly
+ * The sorted halves are adjusted to become a valid bitonic sequence
+ * then merged into an output sorted array
+ *
+ * @author Noha Nomier
+ */
+
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
 public class BitonicStage implements Runnable {
-
     private String threadName;
     private SynchronousQueue<double[]> firstHalfQueue, secondHalfQueue, output;
     private static final int timeout = 10;  // in seconds
@@ -19,19 +27,14 @@ public class BitonicStage implements Runnable {
     }
 
     enum Direction {
-        ASCENDING(0), DESCENDING(1);
-
-        private final int value;
-
-        private Direction(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
+        ASCENDING(), DESCENDING();
     }
 
+    /**
+     * retrieves elements from the firstHalfQueue and secondHalfQueue continuously,
+     * calls process() to perform BitonicSort
+     * and then adds the result to the output queue.
+     */
     @Override
     public void run() {
         double[] halfOne = new double[1];
@@ -42,7 +45,7 @@ public class BitonicStage implements Runnable {
                 halfOne = firstHalfQueue.poll(timeout * 1000, TimeUnit.MILLISECONDS);
                 halfTwo = secondHalfQueue.poll(timeout * 1000, TimeUnit.MILLISECONDS);
 
-                if (halfOne != null && halfTwo !=null) {
+                if (halfOne != null && halfTwo != null) {
                     bitonic = process(halfOne, halfTwo);
                     output.offer(bitonic, timeout * 1000, TimeUnit.MILLISECONDS);
                 } else {
@@ -53,7 +56,13 @@ public class BitonicStage implements Runnable {
             }
         }
     }
-    // merge two sorted arrays (a and b) into a bitonic sequence
+    
+    /**
+     * process the input by merging and sorting it using BitonicSort
+     * @param firstHalf the first half of the input
+     * @param secondHalf the second half of the input
+     * @return the sorted and merged input
+     */
     public double[] process(double[] firstHalf, double[] secondHalf) {
         int totalLength = firstHalf.length + secondHalf.length;
         double[] bitonic = new double[totalLength];
@@ -64,7 +73,7 @@ public class BitonicStage implements Runnable {
 
         // reverse the second input array and copy it to the second half of the bitonic array
         for (int i = 0; i < secondHalf.length; i++)
-            bitonic[i + firstHalf.length] = secondHalf[secondHalf.length - i -1];
+            bitonic[i + firstHalf.length] = secondHalf[secondHalf.length - i - 1];
 
 
         // sort the bitonic array in ascending order
@@ -74,7 +83,13 @@ public class BitonicStage implements Runnable {
     }
 
 
-    // recursive bitonic sort function
+    /**
+     * Recursive method for sorting the input array in bitonic order.
+     * @param arr the input array
+     * @param low the starting index of the portion of the array to be sorted
+     * @param count the number of elements in the portion of the array to be sorted
+     * @param dir the direction of the sort (ascending or descending)
+     */
     private void bitonicSort(double[] arr, int low, int count, Direction dir) {
         if (count > 1) {
             int k = count / 2;
@@ -84,6 +99,13 @@ public class BitonicStage implements Runnable {
         }
     }
 
+    /**
+     * Merges the two halves of the input array in bitonic order.
+     * @param arr the input array
+     * @param low the starting index of the portion of the array to be merged
+     * @param count the number of elements in the portion of the array to be merged
+     * @param dir the direction of the sort (ascending or descending)
+     */
     private void bitonicMerge(double[] arr, int low, int count, Direction dir) {
         for (int i = 0; i < count / 2; i++) {
             compare(arr, low + i, low + (count / 2) + i, dir);
@@ -91,14 +113,20 @@ public class BitonicStage implements Runnable {
 
     }
 
-    // compare two elements and swap them if necessary
+    /**
+     * Compares two elements in the input array and swaps them if necessary.
+     * @param arr the input array
+     * @param i the index of the first element to be compared
+     * @param j the index of the second element to be compared
+     * @param dir the direction of the sort (ascending or descending)
+     */
     private void compare(double[] arr, int i, int j, Direction dir) {
-        if ((dir == Direction.ASCENDING && arr[i] > arr[j]) || (dir == Direction.DESCENDING && arr[i] < arr[j])) {
+        if ((dir == Direction.ASCENDING && arr[i] > arr[j]) ||
+                (dir == Direction.DESCENDING && arr[i] < arr[j])) {
             //swap
             double temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
-
         }
     }
 }
