586c80a3dc12fcff9cefcac675bff74be3bed12a
Noha Nomier <nnomier@Nohas-MacBook-Pro.local>
2023-02-05 16:46:37 -0800

    [Bitonic-Synchronized] Add a static solution for different granularity values

--- HW4/hw4/src/BitonicLoops.java
+++ HW4/hw4/src/BitonicLoops.java
@@ -1,14 +1,25 @@
+import java.util.Map;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
-public class BitonicLoops implements Runnable{
+public class BitonicLoops implements Runnable {
     private int startWire;
     private int endWire;
+    private int threadId;
     private int total_length;
     private double[] data;
+    private CyclicBarrier[] barriers;
+    private Map<Integer, Integer> threadBarrierMap;
 
-
-    private CyclicBarrier barrier;
+    public BitonicLoops(int start, int end, Map<Integer, Integer> threadBarrierMap, CyclicBarrier[] barriers, double[] data, int size, int threadId) {
+        this.startWire = start;
+        this.endWire = end;
+        this.barriers = barriers;
+        this.data = data;
+        this.total_length = size;
+        this.threadBarrierMap = threadBarrierMap;
+        this.threadId = threadId;
+    }
 
     @Override
     public void run() {
@@ -21,26 +32,48 @@ public class BitonicLoops implements Runnable{
         }
     }
 
-    public BitonicLoops(int start, int end, CyclicBarrier barrier, double[] data, int size) {
-        this.startWire = start;
-        this.endWire = end;
-        this.barrier = barrier;
-        this.data = data;
-        this.total_length = size;
-    }
-
     private void sort() throws BrokenBarrierException, InterruptedException {
-        for (int k = 2 ; k <= total_length ; k *=2) {
-            for(int j = k/2 ; j>0 ; j /=2) {
-                barrier.await();
-                for(int i = startWire ; i<endWire ; i++) {
-                    int ixj = i ^j;
+        for (int k = 2; k <= total_length; k *= 2) {
+            for (int j = k / 2; j > 0; j /= 2) {
+                checkBarriers(j);
+                for (int i = startWire; i < endWire; i++) {
+                    int ixj = i ^ j;
                     if (ixj > i) {
                         compare(i, k, data, ixj);
                     }
                 }
             }
         }
+    }
+
+    private void checkBarriers(int j) throws BrokenBarrierException, InterruptedException {
+
+        int granularity = (int) (Math.log(barriers.length + 1) / Math.log(2)); //barriers.length = (1<<granularity)-1
+
+        if (j > 4 || barriers.length == 1) {
+            barriers[0].await(); //biggest barrier in all cases
+        } else if ((j <= 4 && granularity == 2) || (j < 4 && granularity == 3)) {
+            barriers[threadBarrierMap.get(threadId)].await();
+        } else if (j == 4 && granularity == 3) {
+            if (threadId < 4) barriers[1].await();
+            else if (threadId >= 4) {
+                barriers[2].await();
+            }
+
+        }
+//        if(j<4) {
+//            barriers[threadBarrierMap.get(threadId)].await();
+//        } else if (j==4) {
+//            if(threadId < 4) barriers[1].await();
+//            else barriers[2].await();
+//        } else {
+//            barriers[0].await();
+//        }
+//        if(j>4) {
+//            barriers[0].await();
+//        } else {
+//            barriers[threadBarrierMap.get(threadId)+1].await();
+//        }
     }
 
     private void compare(int i, int k, double[] data, int ixj) {
--- HW4/hw4/src/ParallelSorter.java
+++ HW4/hw4/src/ParallelSorter.java
@@ -1,39 +1,88 @@
+/**
+ * 
+ */
+
+import java.util.*;
 import java.util.concurrent.CyclicBarrier;
 
 public class ParallelSorter {
     protected Thread[] workers;
     protected int piece;
-    protected CyclicBarrier barrier;
-    private static final int N_THREADS = 6;
+    protected CyclicBarrier[] barriers;
+    protected Map<Integer, Integer> threadBarrierMap;
+    private static final Set<Integer> ALLOWED_GRANUL = new HashSet<>(Arrays.asList(1, 2, 3));
+    private static final int N_THREADS = 8;
+    private static final int GRANUL = 1; // can be 1, 2, 3
     private static final int N = 1 << 22;
     private static final int TIME_ALLOWED = 10;  // seconds
 
     public ParallelSorter() {
-        barrier = new CyclicBarrier(N_THREADS);
-        piece = N/N_THREADS;
+        barriers = new CyclicBarrier[(1 << GRANUL) - 1];
+        threadBarrierMap = new HashMap<>();
+        initBarriers();
+        piece = N / N_THREADS;
         workers = new Thread[N_THREADS];
+    }
+
+    private void initBarriers() {
+        if (!ALLOWED_GRANUL.contains(GRANUL)) {
+            throw new IllegalArgumentException("Error, The only allowed GRANULARITY values are " +
+                    Arrays.toString(ALLOWED_GRANUL.toArray()));
+        }
+        int nextPowerOfTwo = 1;
+        int barrierIndex = 0;
+        int numberOfThreadsPerBarrier = N_THREADS;
+
+        while (barrierIndex < barriers.length) {
+            barriers[barrierIndex] = new CyclicBarrier(numberOfThreadsPerBarrier);
+            int checkValue = (1 << nextPowerOfTwo) - 2;
+            if (barrierIndex == checkValue && numberOfThreadsPerBarrier > 1) {
+                numberOfThreadsPerBarrier /= 2;
+                nextPowerOfTwo++;
+            }
+            barrierIndex++;
+        }
+//        for (CyclicBarrier b : barriers) {
+//            System.out.println(b.getParties());
+//        }
+
+        fillThreadsMap();
+    }
+
+    private void fillThreadsMap() {
+        int barrierPiece = N_THREADS / GRANUL;
+        for (int i = 0; i < N_THREADS; i++) {
+            int firstPart = ((i / barrierPiece) + GRANUL);
+            if(GRANUL==2) firstPart--;
+
+            int barrierIdx = firstPart % barriers.length;
+
+            threadBarrierMap.put(i, barrierIdx);
+        }
+//        threadBarrierMap.forEach((key, value) -> System.out.println(key + " : " + value));
     }
 
     public static void main(String[] args) throws InterruptedException {
         ParallelSorter parallelSorter = new ParallelSorter();
+
         long start = System.currentTimeMillis();
         int work = 0;
         while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
-        double[] arr = RandomArrayGenerator.getArray(N);
-        for(int i = 0 ; i<N_THREADS ; i++) {
-            int currStart = i *  parallelSorter.piece;
-            int currEnd =  i != N_THREADS - 1 ? currStart + parallelSorter.piece : N;
-            parallelSorter.workers[i] = new Thread(new BitonicLoops(currStart, currEnd, parallelSorter.barrier, arr, N));
-            parallelSorter.workers[i].start();
-        }
+            double[] arr = RandomArrayGenerator.getArray(N);
+            for (int i = 0; i < N_THREADS; i++) {
+                int currStart = i * parallelSorter.piece;
+                int currEnd = i != N_THREADS - 1 ? currStart + parallelSorter.piece : N;
+                parallelSorter.workers[i] = new Thread(new BitonicLoops(currStart, currEnd,parallelSorter.threadBarrierMap, parallelSorter.barriers, arr, N, i));
+                parallelSorter.workers[i].start();
+            }
 
-        for(int i = 0 ; i<N_THREADS ; i++) {
-            parallelSorter.workers[i].join();
-        }
+            for (int i = 0; i < N_THREADS; i++) {
+                parallelSorter.workers[i].join();
+            }
 
-        if(RandomArrayGenerator.isSorted(arr)) work++;
-        else if (!RandomArrayGenerator.isSorted(arr) || N != arr.length)
-            System.out.println("failed");
+            if (RandomArrayGenerator.isSorted(arr)) work++;
+            else if (!RandomArrayGenerator.isSorted(arr) || N != arr.length)
+                System.out.println("failed");
 
         }
 
