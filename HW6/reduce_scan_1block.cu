736f842ae0b05dd32c520b098cfcd257e26c81d0
Noha Nomier <nnomier@Nohas-MacBook-Pro.local>
2023-03-08 17:33:53 -0800

    [CUDA][HW6] Make it work for a size > 1028 by dividing input into block sizes and storing each sum of each block in a separate array  for each block add the sum of previous blocks to make it cumulative

--- HW6/reduce_scan_1block.cu
+++ HW6/reduce_scan_1block.cu
@@ -12,41 +12,41 @@
 using namespace std;
 
 const int MAX_BLOCK_SIZE = 1024;
-const int MAX_ELEMENTS = 4028 // FIXME: change this to 2^20
+const int MAX_ELEMENTS = 4096; // FIXME: change this to 2^20
 
-__global__ void allreduce(float *data) {
-    __shared__ float local[MAX_BLOCK_SIZE]; // 10x faster at least than global memory via data[]
-    int gindex = threadIdx.x;
-    int index = gindex;
-    local[index] = data[gindex];
-    for (int stride = 1; stride < blockDim.x; stride *= 2) {
-
-        __syncthreads();  // wait for my writing partner to put his value in local before reading it
-        int source = (index - stride) % blockDim.x;
-        float addend = local[source];
-
-        __syncthreads();  // wait for my reading partner to pull her value from local before updating it
-        local[index] += addend;
-    }
-    data[gindex] = local[index];
-}
-
-__global__ void scan(float *data) {
+__global__ void blockScan(float *data, float *blockSums) {
     __shared__ float local[MAX_BLOCK_SIZE];
     int gindex = threadIdx.x;
-    int index = gindex;
-    local[index] = data[gindex];
-    for (int stride = 1; stride < blockDim.x; stride *= 2) {
+    int bindex = blockIdx.x;
+    int blockSize = blockDim.x;
+    int index = bindex * blockSize + gindex;
+    local[gindex] = data[index];
+    for (int stride = 1; stride < blockSize; stride *= 2) {
 
         __syncthreads();  // cannot be inside the if-block 'cuz everyone has to call it!
         int addend = 0;
-        if (stride <= index)
-            addend = local[index - stride];
+        if (stride <= gindex)
+            addend = local[gindex - stride];
 
         __syncthreads();
-        local[index] += addend;
+        local[gindex] += addend;
     }
-    data[gindex] = local[index];
+    data[index] = local[gindex];
+    // Store the sum at the last index of the block
+    if (gindex == blockSize - 1) {
+        blockSums[bindex] = local[blockSize-1];
+    }
+}
+
+__global__ void addCumulativeSums(float *data, float *blockSums) {
+    int gindex = threadIdx.x;
+    int bindex = blockIdx.x;
+    int blockSize = blockDim.x;
+    int index = bindex * blockSize + gindex;
+
+    for(int i = 0 ; i<bindex ; i++) {
+        data[index] += blockSums[i];
+    }
 }
 
 void fillArray(float *data, int n, int sz) {
@@ -76,16 +76,26 @@ int main(void) {
     // 	cerr << "Cannot do more than " << threads << " numbers with this simple algorithm!" << endl;
     // 	return 1;
     // }
+    int numBlocks = (MAX_ELEMENTS + threads - 1) / threads;
+    cout<<"BLOCKS"<<numBlocks<<endl;
     cudaMallocManaged(&data, MAX_ELEMENTS * sizeof(*data));
     fillArray(data, n, MAX_ELEMENTS);
     printArray(data, n, "Before");
-    allreduce<<<1, threads>>>(data);
+    float* blockSums;
+    cudaMallocManaged(&blockSums, numBlocks * sizeof(float));
+    // allreduce<<<1, threads>>>(data);
+    // cudaDeviceSynchronize();
+    // printArray(data, n, "Reduce");
+    // fillArray(data, n, threads);
+
+    blockScan<<<numBlocks, threads>>>(data, blockSums);
     cudaDeviceSynchronize();
-    printArray(data, n, "Reduce");
-    fillArray(data, n, threads);
-    scan<<<1, threads>>>(data);
+
+    addCumulativeSums<<<numBlocks, threads>>>(data,blockSums);
     cudaDeviceSynchronize();
+
     printArray(data, n, "Scan");
     cudaFree(data);
+    cudaFree(blockSums);
     return 0;
 }
